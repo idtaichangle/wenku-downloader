4903290454dba6d96750e6983803aff7c01984b8
DESKTOP-ONPCH2S\lixy <lixianyi@cvnavi.com>
2020-04-24 09:43:39 +0800

    fix lddoc bug

--- downloader-startup/src/main/resources/js/lddoc.js
+++ downloader-startup/src/main/resources/js/lddoc.js
@@ -1,6 +1,11 @@
 function getDocType(){ return $("meta[property='og:document:type']").attr("content");}
 function getDocName() {return $("meta[property='og:title']").attr("content");}
-function getPageCount() {return $("meta[property='og:document:page']").attr("content");}
+function getPageCount() {
+    if(typeof ForceFreepage !== "undefined"){
+        return ForceFreepage;
+    }
+    return $("meta[property='og:document:page']").attr("content");
+}
 function getPageWidth() {return $('#outer_page_1').width();}
 function getPageHeight() {return $('#outer_page_1').height();}
 function getPageLeftMargin() {return $('#outer_page_1').offset().left;}
